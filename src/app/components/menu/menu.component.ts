d833fa6fcc269af03688f61b9a03c239f0f89789
Coyla <jcoyla123@gmail.com>
2019-10-02 03:08:36 +0200

    chore: add menu burger section, mobile mode responsive

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -14,6 +14,7 @@ export class MenuComponent implements OnInit {
 
   constructor(private router: Router, private profileService: ProfileService) {
     this.isUserLogged = ProfileService.isUserLogged();
+    this.isUserLogged = true;
   }
 
   ngOnInit() {
